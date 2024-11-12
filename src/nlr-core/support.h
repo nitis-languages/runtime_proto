086f2462246eb86daf2f6d81600525f97a841c26
NiTiSon <NiTiSon@users.noreply.github.com>
2024-11-12 18:14:23 +0300

    add align_up_power2

--- src/nlr-core/support.h
+++ src/nlr-core/support.h
@@ -18,38 +18,84 @@ enum class Result : Sint32 {
 	/// @brief Error during code execution.
 	/// Usually invalid machine code.
 	ExecutionError = -3,
+	/// @brief Parameter value is invalid.
+	InvalidArgument = -4,
 };
 NLR_ENUM_COMPARABLE(Result)
-
-template<typename From, typename To>
-static NLR_INLINE To ptr_cast(From from) noexcept { return (To)from; }
-
-template<typename Func>
-static NLR_INLINE Func ptr_to_func(void *ptr) noexcept { return ptr_cast<void *, Func>(ptr); }
 
 template<typename... Args>
 static NLR_INLINE void unused(Args&&...) noexcept {}
 
+template<typename From, typename To>
+static NLR_INLINE constexpr To ptr_cast(From from) noexcept { return (To)from; }
+
+template<typename Func>
+static NLR_INLINE constexpr Func ptr_to_func(void *ptr) noexcept { return ptr_cast<void *, Func>(ptr); }
+
 template<typename Target>
-static NLR_INLINE Target *move_ptr(void *ptr, SintPtr byte_offset) noexcept {
+static NLR_INLINE constexpr Target *move_ptr(void *ptr, SintPtr byte_offset) noexcept {
 	return ptr_cast<Uint8 *, Target *>(
 		ptr_cast<void *, Uint8 *>(ptr) + byte_offset
 	);
 }
 
 template<typename Lhs, typename Rhs>
-static NLR_INLINE bool has_flag(Lhs value, Rhs flag) noexcept {
+static NLR_INLINE constexpr bool has_flag(Lhs value, Rhs flag) noexcept {
 	return (value & flag) != 0;
 }
 
 template<typename Lhs, typename Rhs>
-static NLR_INLINE bool has_flags(Lhs value, Rhs flags) noexcept {
+static NLR_INLINE constexpr bool has_flags(Lhs value, Rhs flags) noexcept {
 	return (value & flags) == flags;
 }
 
+static NLR_INLINE constexpr Uint8 fill_trailing_bits(Uint8 value) noexcept {
+	value |= value >> 1;
+	value |= value >> 2;
+	value |= value >> 4;
+	return value;
+}
+
+static NLR_INLINE constexpr Uint16 fill_trailing_bits(Uint16 value) noexcept {
+	value |= value >> 1;
+	value |= value >> 2;
+	value |= value >> 4;
+	value |= value >> 8;
+	return value;
+}
+
+static NLR_INLINE constexpr Uint32 fill_trailing_bits(Uint32 value) noexcept {
+	value |= value >> 1;
+	value |= value >> 2;
+	value |= value >> 4;
+	value |= value >> 8;
+	value |= value >> 16;
+	return value;
+}
+
+static NLR_INLINE constexpr Uint64 fill_trailing_bits(Uint64 value) noexcept {
+	value |= value >> 1;
+	value |= value >> 2;
+	value |= value >> 4;
+	value |= value >> 8;
+	value |= value >> 16;
+	value |= value >> 32;
+	return value;
+}
+
 template<typename Integer>
-static NLR_INLINE Integer align_up(Integer value, Integer granularity) noexcept {
-	return ((operand + (granularity - 1)) & ~(granularity - 1));
+static NLR_INLINE constexpr Integer fill_trailing_bits(Integer value) noexcept {
+	return fill_trailing_bits((typename std::make_unsigned<Integer>::type)value);
+}
+
+template<typename Integer>
+static NLR_INLINE constexpr Integer align_up(Integer value, Integer granularity) noexcept {
+	return ((value + (granularity - 1)) & ~(granularity - 1));
+}
+
+template<typename Integer>
+static NLR_INLINE Integer align_up_power2(Integer value) {
+	return fill_trailing_bits<Integer>(value - 1u) + 1u;
 }
 
 NLR_END_NAMESPACE
